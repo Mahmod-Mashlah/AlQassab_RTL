cbb854f8ec8ce8700d62e4d00e9356a82e09ec71
Mahmod Mashlah <mashlahmahmod@gmail.com>
2024-06-18 16:54:09 +0300

    2- add years to headerbar

--- resources/views/layouts/main-headerbar.blade.php
+++ resources/views/layouts/main-headerbar.blade.php
@@ -6,15 +6,15 @@
             <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
-            <a href="index3.html" class="nav-link">Home</a>
+            <a href="{{ route('years') }}" class="nav-link">السنوات الدراسية</a>
         </li>
-        <li class="nav-item d-none d-sm-inline-block">
+        {{-- <li class="nav-item d-none d-sm-inline-block">
             <a href="#" class="nav-link">Contact</a>
-        </li>
+        </li> --}}
     </ul>
 
     <!-- SEARCH FORM -->
-    <form class="form-inline ml-3">
+    {{-- <form class="form-inline ml-3">
         <div class="input-group input-group-sm">
             <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
             <div class="input-group-append">
@@ -23,7 +23,7 @@
                 </button>
             </div>
         </div>
-    </form>
+    </form> --}}
 
     <!-- Right navbar links -->
     <ul class="navbar-nav mr-auto-navbav">
